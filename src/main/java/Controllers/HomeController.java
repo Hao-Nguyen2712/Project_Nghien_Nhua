cb2c647d36f5df20d5ee9d9faee83f9f6227e116
Hao <hao@gmail.com>
2024-01-25 02:53:33 +0700

    Hao : view profile

--- src/main/java/Controllers/HomeController.java
+++ src/main/java/Controllers/HomeController.java
@@ -2,49 +2,56 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controllers;
 
+import DAOs.AccountDAO;
+import Models.Account;
+import Models.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
  * @author nguye
  */
 public class HomeController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet HomeController</title>");  
+            out.println("<title>Servlet HomeController</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet HomeController at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet HomeController at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -52,12 +59,25 @@ public class HomeController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
+            throws ServletException, IOException {
+        String path = request.getRequestURI();
+        HttpSession session =  request.getSession();
+        if (path.endsWith("/HomeController/User")) {
+            AccountDAO adao = new AccountDAO();  
+            User user =  adao.getAccount("joonnguyen2712@gmail.com");
+            session.setAttribute("user", user);
+            request.getRequestDispatcher("/user.jsp").forward(request, response);
+        } else if (path.endsWith("/HomeController/User/Edit")) {
+            AccountDAO adao = new AccountDAO();  
+            User user =  adao.getAccount("joonnguyen2712@gmail.com");
+            session.setAttribute("userRepair", user);
+            request.getRequestDispatcher("/edit.jsp").forward(request, response);
+        } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -65,12 +85,28 @@ public class HomeController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
+            throws ServletException, IOException {
+//         if (request.getParameter("btnUpdate") != null) {
+//            String fulName = request.getParameter("fullName");
+//            String address = request.getParameter("txtAdd");
+//            String phone = request.getParameter("phone");
+//            AccountDAO cDao = new AccountDAO();
+//            Account newAcc = new Account(username, "a", fulName, address, phone);
+//            Account rs = cDao.update(username, newAcc);
+//            if (rs == null) {//them that bai
+//                Account thongtincu = cDao.getAccount(username);
+//                session.setAttribute("account", thongtincu);
+//                response.sendRedirect("/HomeController/User/Edit");
+//            } else {
+//                session.setAttribute("account", newAcc);
+//                response.sendRedirect("/HomeController/User");
+//            }
+//        }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
